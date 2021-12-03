73746f26bb1aed75b4ed61fcf2cddf381d188a64
aenerb <aenerb@gmail.com>
2021-12-03 00:03:46 -0500

    Initial Commit with Day One and Day Two. I'll suffer the large commit later

---
+++ DayOne/AdventDayOne/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventDayOne
+{
+    class Program
+    {
+
+        static int PartOne(string[] input)
+        {
+            int total = 0;
+            for (int count = 1; count < input.Length; count++)
+            {
+                int previous = int.Parse(input[count - 1]);
+                int current = int.Parse(input[count]);
+
+                if (current > previous)
+                {
+                    total++;
+                }
+            }
+            return total;
+        }
+
+        static int PartTwo(string[] input)
+        {
+            int previous = 0;
+            int total = 0;
+            for (int count = 3; count < input.Length; count++)
+            {
+                int first = int.Parse(input[count - 2]);
+                int second = int.Parse(input[count - 1]);
+                int third = int.Parse(input[count]);
+
+                int current = first + second + third;
+                if (current > previous)
+                {
+                    total++;
+                }
+                previous = current;
+            }
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] inputNumbers = System.IO.File.ReadAllLines(@"C:\Projects\AdventCode\DayOne\input.txt");
+
+            Console.WriteLine("Total Part One: " + PartOne(inputNumbers));
+            Console.WriteLine("Total Part Two: " + PartTwo(inputNumbers));
+        }
+    }
+}
---
+++ DayTwo/AdventDayTwo/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventDayTwo
+{
+    class Program
+    {
+        // Part One
+        static int FinalPositionA(string filePath)
+        {
+            int hPos = 0;
+            int vPos = 0;
+            string path = filePath;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] movements = line.Split(' ');
+
+                        var distance = int.Parse(movements[1]);
+
+                        switch (movements[0])
+                        {
+                            case "forward":
+                                hPos += distance;
+                                break;
+                            case "up":
+                                vPos -= distance;
+                                break;
+                            case "down":
+                                vPos += distance;
+                                break;
+                        }
+                    }
+                }
+
+                return hPos * vPos;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+        // Part Two
+        static int FinalPositionB(string filePath)
+        {
+            int hPos = 0;
+            int vPos = 0;
+            int aim = 0;
+            string path = filePath;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] movements = line.Split(' ');
+
+                        var distance = int.Parse(movements[1]);
+
+                        switch (movements[0])
+                        {
+                            case "forward":
+                                hPos += distance;
+                                vPos += (aim * distance);
+                                break;
+                            case "up":
+                                aim -= distance;
+                                break;
+                            case "down":
+                                aim += distance;
+                                break;
+                        }
+                    }
+                }
+
+                return hPos * vPos;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Projects\AdventCode2021\DayTwo\Input.txt";
+
+            var finalPositionA = FinalPositionA(path);
+            var finalPositionB = FinalPositionB(path);
+
+            Console.WriteLine("Part One: " + finalPositionA);
+            Console.WriteLine("Part Two: " + finalPositionB);
+        }
+    }
+}
